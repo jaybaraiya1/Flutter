b0efa4ca2e7aa4c39bba194fc3aa2ecbbc6c5941
jay baraiya <140989637+jaybaraiya1@users.noreply.github.com>
2024-12-06 13:33:14 +0530

    Add files via upload

---
+++ lab_1/Lab_1/Addition.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(){
+  print("enter num 1: ");
+  int a=int.parse(stdin.readLineSync()!);
+  print("enter num 2: ");
+  int b=int.parse(stdin.readLineSync()!);
+  print("add: ${a+b}");
+}
+
